1cd7e7ce29969a19e5741e982b628d2b15640876
Matteo V <matteo.velludini@geo-solutions.it>
2020-03-24 16:20:40 +0100

    Versioning logos and plugins (#72)
    * Versioning logos, header add group, add measure as annotation
    
    * Fix path for logos

--- js/app.jsx
+++ js/app.jsx
@@ -5,7 +5,7 @@
  * This source code is licensed under the BSD-style license found in the
  * LICENSE file in the root directory of this source tree.
  */
-
+require("./customizations.css");
 const ConfigUtils = require('../MapStore2/web/client/utils/ConfigUtils');
 /**
  * Add custom (overriding) translations with:
